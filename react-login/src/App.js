28f6e46c016d0de2c4b26d704948df1ec56ca65d
Charles Mercer <charlesopia@Charless-MBP.local>
2020-02-20 11:02:25 -0500

    added react-router-dom and boostrap packages

---
+++ react-login/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Login from './login.component';
+import SignUp from './signup.component';
+
+function App() {
+return (<Router>
+<div className="App">
+<nav className="navbar navbar-expand-lg navbar-light fixed-top">
+<div className="container">
+<Link className='navbar-brand' to={"/sign-in"}
+)
+}
+
+export default App;
