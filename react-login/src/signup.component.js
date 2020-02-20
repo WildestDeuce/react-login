c0a1b44e16681b56025d2c82f4f2932a089f7d74
Charles Mercer <charlesopia@Charless-MBP.local>
2020-02-20 15:17:20 -0500

    css complete login screen desgined

--- react-login/src/signup.component.js
+++ react-login/src/signup.component.js
@@ -3,9 +3,35 @@ import React, { Component } from 'react';
 export default class SignUp extends Component {
   render() {
     return (
-  <div>
-    <h3>React SignUp Component </h3>
-  </div>
-    )
+      <form>
+        <h3>Sign Up</h3>
+
+        <div className="form-group">
+          <label>First Name</label>
+          <input type="text" className="form-control" placeholder="First Name" />
+        </div>
+
+        <div className="form-group">
+          <label>Last Name</label>
+          <input type="text" className="form-control" placeholder="Last Name" />
+        </div>
+
+
+        <div className="form-group">
+          <label>Email Address</label>
+          <input type="text" className="form-control" placeholder="email" />
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input type="text" className="form-control" placeholder="password" />
+        </div>
+
+        <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+        <p className="forgot-password text-right">
+          Already registered <a href="#">Sign In></a>
+        </p>
+      </form>      
+    );
   }
 }
