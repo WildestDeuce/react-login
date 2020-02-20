db663a94a0648c329fd948efb96588fc2a471e70
Charles Mercer <charlesopia@Charless-MBP.local>
2020-02-20 13:57:25 -0500

    Added Routes to App.js

--- react-login/src/App.js
+++ react-login/src/App.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Login from './login.component';
 import SignUp from './signup.component';
 
 function App() {
-return (<Router>
-<div className="App">
-<nav className="navbar navbar-expand-lg navbar-light fixed-top">
-<div className="container">
-<Link className='navbar-brand' to={"/sign-in"}
-)
+	return (<Router>
+		<div className="App">
+			<nav className="navbar navbar-expand-lg navbar-light fixed-top">
+				<div className="container">
+					<Link className="navbar-brand" to={'sign-in'}>positronX.io</Link>
+					<div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+						<ul className="navbar-nav ml-auto">
+							<li classname="nav-item">
+								<Link className="nav-link" to={'sign-in'}>Login</Link>
+							</li>
+							<li className="nav-item">
+								<Link className="nav-link" to={'sign-up'}>Sign</Link>
+							</li>
+
+						</ul>
+					</div>
+				</div>
+			</nav>
+			<div className="auth-wrapper">
+				<div className="auth-inner">
+<Switch>
+<Route exact path='/' component={Login} />
+<Route exact path='/sign-in'component={Login} />
+<Route exact path='/sign-up' component={SignUp} />
+
+
+
+</Switch>
+
+
+				</div>
+
+
+			</div>
+		</div>
+	</Router>)
 }
+
+
+
+
+
+
+
+
+
+
+
 
 export default App;
