66855207f3ab9bcb0689bda3256714f9cec05e55
Charles Mercer <charlesopia@Charless-MBP.local>
2020-02-20 14:44:35 -0500

    login form functions needs to be styled

--- react-login/src/App.js
+++ react-login/src/App.js
@@ -6,35 +6,39 @@ import Login from './login.component';
 import SignUp from './signup.component';
 
 function App() {
-	return (<Router>
-		<div className="App">
-			<nav className="navbar navbar-expand-lg navbar-light fixed-top">
-				<div className="container">
-					<Link className="navbar-brand" to={'sign-in'}>positronX.io</Link>
-					<div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-						<ul className="navbar-nav ml-auto">
-							<li classname="nav-item">
-								<Link className="nav-link" to={'sign-in'}>Login</Link>
-							</li>
-							<li className="nav-item">
-								<Link className="nav-link" to={'sign-up'}>Sign</Link>
-							</li>
+	return (
+		
+			<Router>
+				<div className="App">
+					<nav className="navbar navbar-expand-lg navbar-light fixed-top">
+						<div className="container">
+							<Link className="navbar-brand" to={'sign-in'}>positronX.io</Link>
+							<div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+								<ul className="navbar-nav ml-auto">
+									<li classname="nav-item">
+										<Link className="nav-link" to={'sign-in'}>Login</Link>
+									</li>
+									<li className="nav-item">
+										<Link className="nav-link" to={'sign-up'}>Sign</Link>
+									</li>
 
-						</ul>
+								</ul>
+							</div>
+						</div>
+					</nav>
+					<div className="auth-wrapper">
+						<div className="auth-inner">
+							<Switch>
+								<Route exact path='/' component={Login} />
+								<Route exact path='/sign-in' component={Login} />
+								<Route exact path='/sign-up' component={SignUp} />
+							</Switch>
+						</div>
 					</div>
 				</div>
-			</nav>
-			<div className="auth-wrapper">
-				<div className="auth-inner">
-					<Switch>
-						<Route exact path='/' component={Login} />
-						<Route exact path='/sign-in' component={Login} />
-						<Route exact path='/sign-up' component={SignUp} />
-					</Switch>
-				</div>
-			</div>
-		</div>
-	</Router>
+			</Router>
+		
+
 	);
 }
 export default App;
--- react-login/src/login.component.js
+++ react-login/src/login.component.js
@@ -1,11 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class Login extends Component {
-render() {
-  return(
-    <div>
-    <h3> React Login Component </h3>
-    </div>
-  );
-}
+  render() {
+    return (
+      <form>
+        <h3>Sign In</h3>
+        <div className="form-group">
+          <label>Email address</label>
+          <input type="email" className="form-control" placeholder="Eneter email"></input>
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" className="form-control" placeholder="Enter password" />
+        </div>
+        <div className="form-group">  
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className='custom-control-input' id="customCheck1"></input>
+            <label className="custom-control-label" htmlFor="customCheck1">Remember Me</label>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary btn-block"></button>
+        <p className="forgot-password text-right">
+          Forgot <a href="#">password?</a>
+        </p>
+      </form>
+
+    );
+  }
 }
