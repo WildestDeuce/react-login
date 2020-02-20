c90a1b7e955e2b3f32147a478e4107f2c460b4d3
Charles Mercer <charlesopia@Charless-MBP.local>
2020-02-20 15:21:23 -0500

    cleaned up UI error

--- react-login/src/signup.component.js
+++ react-login/src/signup.component.js
@@ -29,7 +29,7 @@ export default class SignUp extends Component {
 
         <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
         <p className="forgot-password text-right">
-          Already registered <a href="#">Sign In></a>
+          Already registered <a href="#">Sign In</a>
         </p>
       </form>      
     );
